f0b3a72018fc228d10076e8743a1c259e1b58bb0
NyanPero <hiroki.nakano.826@gmail.com>
2021-02-04 21:10:53 +0900

    modify selecting merge file paths

--- src/merge_data.py
+++ src/merge_data.py
@@ -20,29 +20,13 @@ def merge_dataframe(jsonl_files):
     concatenated_df = pd.concat(df_list, axis=0)
     return concatenated_df.drop_duplicates(subset="url")
 
-def extract_jsonl_list(since_time, until_time, file_names):
-    
-    check_jsonl_list = []
-    
-    for file_name in file_names:
-        str_collect_datetime = str(file_name).split("/")[-1].split("_")[0]
-        dt_collect_datetime = datetime.datetime.strptime(str_collect_datetime, '%Y-%m-%dT%H:%M:%S')
-        dt_since_datetime = datetime.datetime.strptime(since_time, '%Y-%m-%dT%H:%M:%S')
-        dt_until_datetime = datetime.datetime.strptime(until_time, '%Y-%m-%dT%H:%M:%S')
-        if dt_since_datetime <= dt_collect_datetime <= dt_until_datetime:
-            check_jsonl_list.append(file_name)
-    
-    return check_jsonl_list
-
-
 @click.command()
-@click.option('--since-time')
-@click.option('--until-time')
+@click.option('--input-time')
 @click.option('--output-dir')
-def merge_datasets(since_time, until_time, output_dir):
+def merge_datasets(input_time, output_dir):
     # extract jsonl files
-    file_names = Path(output_dir).glob('*.jsonl')
-    check_jsonl_list = extract_jsonl_list(since_time, until_time, file_names)
+    input_time_str = "_".join(input_time.split("/")[-1].split("_")[0:2])
+    check_jsonl_list = Path(output_dir).glob('{}*.jsonl'.format(input_time_str))
     
     # merge dataframe
     df = merge_dataframe(check_jsonl_list)
