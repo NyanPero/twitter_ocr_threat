82741c8610b0b4cb366e85063c80486b1d992c4e
NyanPero <hiroki.nakano.826@gmail.com>
2021-02-05 13:56:46 +0900

    orc -> ocr

--- src/extract_text.py
+++ src/extract_text.py
@@ -106,7 +106,7 @@ def extract_texts(input_file, output_dir, source_dir):
     ocr_series = pd.Series(text_list, index=df.index, name='ocr_text')
     new_df = pd.concat([df,ocr_series], axis=1)
     new_df_dropped = new_df[new_df.astype(str)['ocr_text'] != '[]']
-    file_name = input_file.replace('.jsonl','_orc.jsonl')
+    file_name = input_file.replace('.jsonl','_ocr.jsonl')
     save_datasets(new_df_dropped, Path(output_dir), file_name)
 
 
